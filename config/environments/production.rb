23fad1b243bbc3fa023cd0c5ab69bdbdb0931347
Michael <mikemach123@gmail.com>
2017-01-11 10:28:16 -0800

    set configs to true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.assets.initialize_on_precompile = false
+  config.serve_static_assets = true
 
   # Settings specified here will take precedence over those in config/application.rb.
   config.serve_static_files = true
@@ -25,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
